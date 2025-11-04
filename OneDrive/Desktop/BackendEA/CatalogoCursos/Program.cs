3581d2fe7ea8e61174f532203f17ee4c7e1836e3
EArana <eaarana@uamv.edu.ni>
2025-11-04 09:36:11 -0600

    Versión inicial del proyecto CatalogoCursos: estructura base, clase Program y verificación de ejecución

---
+++ OneDrive/Desktop/BackendEA/CatalogoCursos/Program.cs
@@ -0,0 +1,15 @@
+﻿//Codigo generado con ayuda de gitHub Copilot
+using System;
+
+namespace CatalogoCursos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CATÁLOGO DE CURSOS ===");
+            Console.WriteLine("Proyecto inicializado correctamente");
+        }
+    }
+}
+
